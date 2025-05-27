99c2c300d518c496d62f936d074f9a2ad3e61950
craguila14 <craguilaasenjo@gmail.com>
2025-05-26 20:12:31 -0400

    env files in gitignore

--- src/app/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-    companyName: 'Gifs',
-    companyName2: 'App',
-    companySlogan: 'Maneja tus gifs',
-
-    giphyApiKey: 'TvZLCTqPvl2vhjmZU7u0f4IJpeg6vFOS',
-     giphyUrl: 'https://api.giphy.com/v1'
-}
